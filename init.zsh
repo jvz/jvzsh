29541193b7472ecc06db866d53e6025a4f1db9d2
Matt Sicker <boards@gmail.com>
2020-04-16 17:31:34 -0500

    Combine alias functions into aliases

--- init.zsh
+++ init.zsh
@@ -5,4 +5,10 @@
 # any custom stuff should go here.
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
-
+alias bubu='brew upgrade && brew cask upgrade'
+alias docker-cleanup-images='docker image prune'
+alias docker-remove-stopped='docker container prune'
+alias gdmb='git delete-merged-branches'
+alias la='ls -A'
+alias reload='rm -f ~/.zcompdump && source ~/.zshrc && compinit'
+alias rmrf='rm -rf'
