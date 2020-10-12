e06c5c792966a678e26e0e9a13a2683df9e2b10e
Matt Sicker <boards@gmail.com>
2020-10-12 10:16:14 -0500

    Update brew alias

--- init.zsh
+++ init.zsh
@@ -5,10 +5,11 @@
 # any custom stuff should go here.
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
-alias bubu='brew upgrade && brew cask upgrade'
+alias bubu='brew upgrade && brew upgrade --cask'
 alias docker-cleanup-images='docker image prune'
 alias docker-remove-stopped='docker container prune'
 alias gdmb='git delete-merged-branches'
 alias la='ls -A'
 alias reload='rm -f ~/.zcompdump; source ~/.zshrc; compinit'
 alias rmrf='rm -rf'
+alias gh='git'
